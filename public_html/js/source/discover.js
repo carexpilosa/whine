73f278409e03bdbad781ed804ebe846f85433011
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-06-29 16:50:31 +0200

    dev:Suggest

--- public_html/js/source/discover.js
+++ public_html/js/source/discover.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
 import Button from './components/Button';
+import Suggest from './components/Suggest';
 
 ReactDOM.render(
   <div style={ {padding: '20px'} }>
@@ -16,6 +17,17 @@ ReactDOM.render(
       <h2>Buttons</h2>
       <div>Button mit onClick: 
         <Button onClick={() => alert('Aua')}>Klick mich</Button>
+      </div>
+      <div>Ein Link:
+        <Button href="http://reactjs.com">Folge mir</Button>
+      </div>
+      <div>Eigener Klassenname:
+        <Button className="custom">Ich mache nichts</Button>
+      </div>
+      
+      <h2>Suggest</h2>
+      <div>
+        <Suggest options={['eins', 'zwei', 'dro', 'dreißig']} />
       </div>
       {/* weitere Komponenten */}
   </div>,
