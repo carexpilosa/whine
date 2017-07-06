4e0e51047f393075dcf639e254128a49831b6979
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-07-07 00:31:57 +0200

    dev:Excel - still faults ...

--- public_html/js/source/app.js
+++ public_html/js/source/app.js
@@ -1,25 +1,26 @@
-'use strict';
+"use strict";
 
-var headers = localStorage.getItem('headers');
-//var data = localStorage.getItem('data');
-var data = [];
-
-if (! headers) {
-  headers = ['Titel', 'Jahr', 'Bewertung', 'Kommentar'];
-  data = [['Test', '2015', '3', 'naja']];
-}
-
+import Logo from './components/Logo';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Logo from './components/Logo';
-import Excel from './components/Excel';
+//import Whinepad from './components/Whinepad';
+import schema from './schema';
+
+let data = JSON.parse(localStorage.getItem('data'));
+
+// default example data, read from the schema
+if (!data) {
+  data = {};
+  schema.forEach((item) => data[item.id] = item.sample);
+  data = [data];
+}
 
 ReactDOM.render(
   <div>
-    <h1>
-      <Logo />Willkommen zur Applikation
-    </h1>
-    <Excel headers={headers} initialData={data} />
+    <div className="app-header">
+      <Logo/> Welcome to Whinepad!
+    </div>
+    {/*<Whinepad schema={schema} initialData={data} />*/}
   </div>,
   document.getElementById('pad')
 );
